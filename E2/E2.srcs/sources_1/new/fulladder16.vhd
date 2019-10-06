ef0b55900782d923cc80a600d6c3884dac880236
juan <jaromero6@uc.cl>
2019-10-06 17:33:04 -0300

    :construction: Create sumator

---
+++ E2/E2.srcs/sources_1/new/fulladder16.vhd
@@ -0,0 +1,109 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03.10.2019 01:25:29
+-- Design Name: 
+-- Module Name: ALU - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity ALU is
+    Port ( A : in STD_LOGIC_VECTOR (15 downto 0);
+           B : in STD_LOGIC_VECTOR (15 downto 0);
+           carryIn: in STD_LOGIC;
+           dataOut : out STD_LOGIC_VECTOR (15 downto 0);
+           C : out STD_LOGIC;
+           Z : out STD_LOGIC;
+           N : out STD_LOGIC);
+end ALU;
+
+architecture Behavioral of ALU is
+    signal carry1 : STD_LOGIC;
+    signal carry2 : STD_LOGIC;
+    signal carry3 : STD_LOGIC;
+    signal carry4 : STD_LOGIC;
+    signal carry5 : STD_LOGIC;
+    signal carry6 : STD_LOGIC;
+    signal carry7 : STD_LOGIC;
+    signal carry8 : STD_LOGIC;
+    signal carry9 : STD_LOGIC;
+    signal carry10 : STD_LOGIC;
+    signal carry11 : STD_LOGIC;
+    signal carry12 : STD_LOGIC;
+    signal carry13 : STD_LOGIC;
+    signal carry14 : STD_LOGIC;
+    signal carry15 : STD_LOGIC;
+
+begin
+    -- Bit 1
+    dataOut(0) <= (B(0) xor carryIn) xor A(0); 
+    -- Bit 2
+    carry1 <= (B(0) and carryIn) or (A(0) and carryIn) or (A(0) and B(0));
+    dataOut(1) <= (B(1) xor carry1) xor A(1);
+    -- Bit 3
+    carry1 <= (B(0) and carryIn) or (A(0) and carryIn) or (A(0) and B(0));
+    dataOut(1) <= (B(1) xor carry1) xor A(1);
+    -- Bit 4
+    carry1 <= (B(0) and carryIn) or (A(0) and carryIn) or (A(0) and B(0));
+    dataOut(1) <= (B(1) xor carry1) xor A(1);
+    -- Bit 5
+    carry1 <= (B(0) and carryIn) or (A(0) and carryIn) or (A(0) and B(0));
+    dataOut(1) <= (B(1) xor carry1) xor A(1);
+    -- Bit 6
+    carry1 <= (B(0) and carryIn) or (A(0) and carryIn) or (A(0) and B(0));
+    dataOut(1) <= (B(1) xor carry1) xor A(1);
+    -- Bit 7
+    carry1 <= (B(0) and carryIn) or (A(0) and carryIn) or (A(0) and B(0));
+    dataOut(1) <= (B(1) xor carry1) xor A(1);
+    -- Bit 8
+    carry1 <= (B(0) and carryIn) or (A(0) and carryIn) or (A(0) and B(0));
+    dataOut(1) <= (B(1) xor carry1) xor A(1);
+    -- Bit 9
+    carry1 <= (B(0) and carryIn) or (A(0) and carryIn) or (A(0) and B(0));
+    dataOut(1) <= (B(1) xor carry1) xor A(1);
+    -- Bit 10
+    carry1 <= (B(0) and carryIn) or (A(0) and carryIn) or (A(0) and B(0));
+    dataOut(1) <= (B(1) xor carry1) xor A(1);
+    -- Bit 11
+    carry1 <= (B(0) and carryIn) or (A(0) and carryIn) or (A(0) and B(0));
+    dataOut(1) <= (B(1) xor carry1) xor A(1);
+   -- Bit 12
+    carry1 <= (B(0) and carryIn) or (A(0) and carryIn) or (A(0) and B(0));
+    dataOut(1) <= (B(1) xor carry1) xor A(1);
+    -- Bit 13
+    carry1 <= (B(0) and carryIn) or (A(0) and carryIn) or (A(0) and B(0));
+    dataOut(1) <= (B(1) xor carry1) xor A(1);
+    -- Bit 14
+    carry1 <= (B(0) and carryIn) or (A(0) and carryIn) or (A(0) and B(0));
+    dataOut(1) <= (B(1) xor carry1) xor A(1);
+    -- Bit 15
+    carry1 <= (B(0) and carryIn) or (A(0) and carryIn) or (A(0) and B(0));
+    dataOut(1) <= (B(1) xor carry1) xor A(1);
+    -- Bit 16
+    carry1 <= (B(0) and carryIn) or (A(0) and carryIn) or (A(0) and B(0));
+    dataOut(1) <= (B(1) xor carry1) xor A(1);
+end Behavioral;
