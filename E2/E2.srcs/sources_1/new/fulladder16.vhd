4a606840af4617651b428aa28a0a81efef502746
juan <jaromero6@uc.cl>
2019-10-06 17:46:19 -0300

    :bug: Fixed 3 bit

--- E2/E2.srcs/sources_1/new/fulladder16.vhd
+++ E2/E2.srcs/sources_1/new/fulladder16.vhd
@@ -66,8 +66,8 @@ begin
     dataOut(1) <= (B(1) xor carry1) xor A(1);
     carry2 <= (B(1) and carry1) or (A(1) and carry1) or (A(1) and B(1));
     -- Bit 3
-    carry3 <= (B(2) and carry2) or (A(2) and carry2) or (A(2) and B(2));
     dataOut(2) <= (B(2) xor carry2) xor A(2);
+    carry3 <= (B(2) and carry2) or (A(2) and carry2) or (A(2) and B(2));
     -- Bit 4
     carry4 <= (B(3) and carry3) or (A(3) and carry3) or (A(3) and B(3));
     dataOut(3) <= (B(3) xor carry2) xor A(3);
