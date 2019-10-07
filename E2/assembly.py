4a10b8349c10d60598c3a2ff82f108046e4c1be8
juan <jaromero6@uc.cl>
2019-10-07 15:31:50 -0300

    :construction: Added dictionary for instructions

---
+++ E2/assembly.py
@@ -0,0 +1,9 @@
+from instructions import INSTRUCTIONS as I
+
+
+def parse_instruction(instruction):
+    pass
+
+def get_literal(instruction):
+    pass
+
---
+++ E2/instructions.py
@@ -0,0 +1,20 @@
+INSTRUCTIONS = {
+    "MOV" :{},
+    "ADD" :{},
+    "SUB" :{},
+    "ADD" :{},
+    "AND": {},
+    "OR": {},
+    "XOR": {},
+    "SHL": {},
+    "NOT": {},
+    "SHR": {},
+    "INC": {},
+    "DEC": {},
+    "CMP": {},
+    "JMP": {},
+    "JEQ": {},
+    "JNE": {},
+    "NOP": {}
+
+}
