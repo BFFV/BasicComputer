1be9a037afcd9609bf6bbff4b2a7456d171a0222
juan <jaromero6@uc.cl>
2019-10-07 19:27:55 -0300

    :bug: Fixed errors in ALU

--- E2/E2.srcs/sources_1/new/ALU.vhd
+++ E2/E2.srcs/sources_1/new/ALU.vhd
@@ -42,8 +42,42 @@ entity ALU is
 end ALU;
 
 architecture Behavioral of ALU is
-
+    signal oneComplement : STD_LOGIC;
+    signal cFulladder : STD_LOGIC;
+    signal nFulladder : STD_LOGIC;
+    signal zFulladder : STD_LOGIC;
+    signal resultFulladder: STD_LOGIC_VECTOR(15 downto 0);
+   component fulladder is
+    Port ( A : in STD_LOGIC_VECTOR (15 downto 0);
+           regB : in STD_LOGIC_VECTOR (15 downto 0);
+           carryIn : in STD_LOGIC;
+           result : out STD_LOGIC_VECTOR (15 downto 0);
+           C : out STD_LOGIC;
+           Z : out STD_LOGIC;
+           N : out STD_LOGIC);
+end component;
 begin
+   --- Se conectan los puestos
+   G1: fulladder port map (A => A,
+                           regB => B,
+                           carryIn => oneComplement,  
+                           C => cFulladder,
+                           Z => zFulladder,
+                           N => nFulladder,
+                           result => resultFulladder);
+
+   --- Se definen variables que se puedan necesitar para cada modulo
+   oneComplement <= selALU(0);
 
 
+  --- Aquí se realiza la asignacion da los datos de salida de la ALU
+   with selALU select
+        dataOut <= resultFulladder when "000", resultFulladder when "001", "0000000000000000" when others;
+   with selALU select
+        C <= '0' when "000", 
+        cFulladder when "001", '0' when others;
+   with selALU select
+        Z <= zFulladder when "000", zFulladder when "001", '0' when others;
+   with selALU select
+        N <= nFulladder when "000", nFulladder when "001", '0' when others;
 end Behavioral;
