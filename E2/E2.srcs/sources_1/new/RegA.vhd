e21600df4cc81e0df4e2c2817d811d5d25d6b368
jgparot <jgparot@uc.cl>
2019-10-10 16:49:59 -0300

    PC , RegA y RegB

--- E2/E2.srcs/sources_1/new/RegA.vhd
+++ E2/E2.srcs/sources_1/new/RegA.vhd
@@ -36,14 +36,47 @@ entity RegA is
            clock : in STD_LOGIC;
            dataIn : in STD_LOGIC_VECTOR (15 downto 0);
            dataOut : out STD_LOGIC_VECTOR (15 downto 0);
-           selA : in STD_LOGIC_VECTOR (1 downto 0);
-           seg : out STD_LOGIC_VECTOR (7 downto 0);
-           an : out STD_LOGIC_VECTOR (3 downto 0));
+           selA : in STD_LOGIC_VECTOR (1 downto 0));
 end RegA;
 
 architecture Behavioral of RegA is
 
-begin
+component Reg is
+    Port ( clock    : in  std_logic;                        -- Señal del clock (reducido).
+           load     : in  std_logic;                        -- Señal de carga.
+           up       : in  std_logic;                        -- Señal de subida.
+           down     : in  std_logic;                        -- Señal de bajada.
+           datain   : in  std_logic_vector (15 downto 0);   -- Señales de entrada de datos.
+           dataout  : out std_logic_vector (15 downto 0));  -- Señales de salida de datos.
+end component;
 
+component mux_a is
+    Port (  
+      A      : in  std_logic_vector(15 downto 0);
+      SelA     : in  std_logic_vector(1 downto 0);
+      DataOut       : out std_logic_vector(15 downto 0) );
+end component;
+
+signal mux_out : std_logic_vector(15 downto 0);
+signal reg_out : std_logic_vector(15 downto 0);
+
+
+begin
+ 
+Ra: Reg port map (
+           clock => clock,
+           load => loadA,
+           up => '0'    ,
+           down => '0'   ,
+           datain => DataIn  ,
+           dataout => reg_out
+                    ); 
+
+Ma: mux_a port map(
+                    A => reg_out,
+                    selA => selA,                 
+                    DataOut => mux_out
+                       ); 
+dataout <= mux_out;
 
 end Behavioral;
--- E2/E2.srcs/sources_1/new/RegB.vhd
+++ E2/E2.srcs/sources_1/new/RegB.vhd
@@ -38,14 +38,53 @@ entity RegB is
            dataOut : out STD_LOGIC_VECTOR (15 downto 0);
            selB : in STD_LOGIC_VECTOR (1 downto 0);
            memIn : in STD_LOGIC_VECTOR (15 downto 0);
-           lit : in STD_LOGIC_VECTOR (15 downto 0);
-           seg : out STD_LOGIC_VECTOR (7 downto 0);
-           an : out STD_LOGIC_VECTOR (3 downto 0));
+           lit : in STD_LOGIC_VECTOR (15 downto 0));
+           
 end RegB;
 
 architecture Behavioral of RegB is
 
+component Reg is
+    Port ( clock    : in  std_logic;                        -- Señal del clock (reducido).
+           load     : in  std_logic;                        -- Señal de carga.
+           up       : in  std_logic;                        -- Señal de subida.
+           down     : in  std_logic;                        -- Señal de bajada.
+           datain   : in  std_logic_vector (15 downto 0);   -- Señales de entrada de datos.
+           dataout  : out std_logic_vector (15 downto 0));  -- Señales de salida de datos.
+end component;
+
+component mux_b is
+ Port (  
+      B      : in  std_logic_vector(15 downto 0);
+      SelB     : in  std_logic_vector(1 downto 0);
+      Lit      : in std_logic_vector(15 downto 0);
+      DRam     : in std_logic_vector(15 downto 0);
+      DataOut       : out std_logic_vector(15 downto 0) );
+end component;
+
+signal mux_out : std_logic_vector(15 downto 0);
+signal reg_out : std_logic_vector(15 downto 0);
+
+
 begin
+ 
+Ra: Reg port map (
+           clock => clock,
+           load => loadB,
+           up => '0'    ,
+           down => '0'   ,
+           datain => DataIn  ,
+           dataout => reg_out
+                    ); 
+
+Ma: mux_b port map(
+                    B => reg_out,
+                    Dram => memin, 
+                    selB => selB,
+                    lit => lit,                 
+                    DataOut => mux_out
+                       ); 
+dataout <= mux_out;
 
 
 end Behavioral;
